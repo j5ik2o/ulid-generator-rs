468bba341c19309778c35d7792d4544efa3e7d97
Junichi Kato <j5ik2o@gmail.com>
2021-01-03 16:09:25 +0900

    Update rustdoc

--- src/serde.rs
+++ src/serde.rs
@@ -6,11 +6,20 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! Serialization and deserialization.
+//!
+//! By default, serialization and deserialization go through ULID's 26-character
+//! canonical string representation as set by the ULID standard.
+//!
+//! ULIDs can optionally be serialized as u128 integers using the `ulid_as_u128`
+//! module. See the module's documentation for examples.
+
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use uuid::Uuid;
 
 use crate::ULID;
 
+/// Default Serializer
 impl Serialize for ULID {
   fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
   where
@@ -20,6 +29,7 @@ impl Serialize for ULID {
   }
 }
 
+/// Default Deserializer
 impl<'de> Deserialize<'de> for ULID {
   fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
   where
@@ -29,6 +39,25 @@ impl<'de> Deserialize<'de> for ULID {
   }
 }
 
+/// Serialization and deserialization of ULIDs through their inner u128 type.
+///
+/// To use it, annotate a field with
+/// `#[serde(with = "ulid_as_u128")]`,
+/// `#[serde(serialize_with = "ulid_as_u128")]`, or
+/// `#[serde(deserialize_with = "ulid_as_u128")]`.
+///
+/// # Examples
+///
+/// ```
+/// # use ulid_generator_rs::ULID;
+/// # use ulid_generator_rs::serde::ulid_as_u128;
+/// # use serde::{Serialize, Deserialize};
+/// #[derive(Serialize, Deserialize)]
+/// struct U128Example {
+///     #[serde(with = "ulid_as_u128")]
+///     identifier: ULID
+/// }
+/// ```
 pub mod ulid_as_u128 {
   use super::*;
 
@@ -48,6 +77,25 @@ pub mod ulid_as_u128 {
   }
 }
 
+/// Serialization and deserialization of ULIDs through UUID strings.
+///
+/// To use this module, annotate a field with
+/// `#[serde(with = "ulid_as_uuid")]`,
+/// `#[serde(serialize_with = "ulid_as_uuid")]`, or
+/// `#[serde(deserialize_with = "ulid_as_uuid")]`.
+///
+/// # Examples
+///
+/// ```
+/// # use ulid_generator_rs::ULID;
+/// # use ulid_generator_rs::serde::ulid_as_uuid;
+/// # use serde::{Serialize, Deserialize};
+/// #[derive(Serialize, Deserialize)]
+/// struct UuidExample {
+///     #[serde(with = "ulid_as_uuid")]
+///     identifier: ULID
+/// }
+/// ```
 #[cfg(all(feature = "uuid", feature = "serde"))]
 pub mod ulid_as_uuid {
   use super::*;
